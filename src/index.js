7df2bd3a391560b1da8d69568814596a7825b9b1
Rahul Mathews <rahulmudulakar123@gmail.com>
2019-12-28 22:49:49 +0530

    Cleaned up messy code and reconfigured

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-//Global Styles
-
-import "./css/globals/flaticon.css";
-import "./css/globals/menu.css";
-import "./css/globals/dropdown-effects/fade-down.css";
-import "./css/globals/magnific-popup.css";	
-import "./css/globals/owl.carousel.min.css";
-import "./css/globals/owl.theme.default.min.css";
-import "./css/globals/animate.css";
-import "./css/globals/jquery.datetimepicker.min.css";
-        
-import "./css/globals/style.css";
-import "./css/globals/responsive.css"; 
-
-import "./css/globals/bootstrap.min.css";
-
         
 //Main Container
 import App from './container/App';
